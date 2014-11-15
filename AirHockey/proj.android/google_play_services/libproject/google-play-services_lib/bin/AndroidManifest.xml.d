bcbe9a08262fc7c7cbd7a429638eb02f0e6132d1
Seryusjj <sergiojj932@gmail.com>
2014-11-15 18:52:08 +0100

    play services

--- AirHockey/proj.android/google_play_services/libproject/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/sergio/GitHub/Cocos2D-x-V3.2/AirHockey/proj.android/google_play_services/libproject/google-play-services_lib/bin/AndroidManifest.xml : \
-/home/sergio/GitHub/Cocos2D-x-V3.2/AirHockey/proj.android/google_play_services/libproject/google-play-services_lib/AndroidManifest.xml \
